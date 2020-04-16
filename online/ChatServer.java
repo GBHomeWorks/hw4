67e507026df872ccf3d89c67cc354b652a841cb2
GBHomeWorks <a.a.mikhaylov2015@gmail.com>
2020-04-16 14:03:16 +0300

    Initial commit

---
+++ online/ChatServer.java
@@ -0,0 +1,12 @@
+package online;
+
+public class ChatServer {
+    public void start(int port) {
+        System.out.println("Server started at port: " + port);
+    }
+
+    public void stop() {
+        System.out.println("Server stopped");
+    }
+
+}
